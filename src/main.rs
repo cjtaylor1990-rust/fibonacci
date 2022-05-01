218eb964740359148c59c3d27a68cf8b650fcd72
cjtaylor1990 <9288608+cjtaylor1990@users.noreply.github.com>
2022-05-01 08:24:03 -0400

    Adding basic fibonacci calculator.
    Adding basic fibonacci calculator using a for loop.
    
    This is part of Ch.3 of TRPL.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    test_fibonacci();
+}
+
+fn fibonacci(n: u32) -> u32 {
+    let mut previous = 1;
+    let mut current = 1;
+
+    let mut temp;
+    for _n in 3..n+1 {
+        temp = current;
+        current = current + previous;
+        previous = temp;
+    }
+    current
+}
+
+fn test_fibonacci() {
+    println!("First: {}", fibonacci(1));
+    println!("Second: {}", fibonacci(2));
+    println!("Third: {}", fibonacci(3));
+    println!("Fourth: {}", fibonacci(4));
+    println!("Fifth: {}", fibonacci(5));
+    println!("Sixth: {}", fibonacci(6));
+    println!("Seventh: {}", fibonacci(7));
+    println!("Eighth: {}", fibonacci(8));
+    println!("Ninth: {}", fibonacci(9));
+    println!("Tenth: {}", fibonacci(10));
 }
