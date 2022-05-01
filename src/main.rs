5009b5dc901b935bf2f6d36b2bc6efd3c7c527d6
cjtaylor1990 <9288608+cjtaylor1990@users.noreply.github.com>
2022-05-01 16:14:20 -0400

    Changing functions to be 0-indexed.
    Changing fibonacci and fibonacci_
    recursive to start at 0. Changing
    the test function accordingly.
    
    This is part of Ch.3 of TRPL.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ fn main() {
     // Testing recursive fibonacci
     println!("Recursive fibonacci calculation:");
     test_fibonacci(fibonacci_recursive);
-
 }
 
 fn fibonacci(n: u32) -> u32 {
@@ -15,7 +14,7 @@ fn fibonacci(n: u32) -> u32 {
     let mut current = 1;
 
     let mut temp;
-    for _n in 3..n+1 {
+    for _n in 2..n+1 {
         temp = current;
         current = current + previous;
         previous = temp;
@@ -25,7 +24,7 @@ fn fibonacci(n: u32) -> u32 {
 
 fn fibonacci_recursive(n: u32) -> u32 {
     // Fibonacci calculation using recursion.
-    if n == 1 || n == 2 {
+    if n == 0 || n == 1 {
         return 1
     }
     fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
@@ -34,18 +33,17 @@ fn fibonacci_recursive(n: u32) -> u32 {
 fn test_fibonacci(fn_to_test: fn(u32) -> u32) {
     /*
     * For a given fibonacci calculator, print the
-    * first 10 fibonacci numbers.
+    * first 10 fibonacci numbers (counting in 0 index).
     *
     * For the i-th number, the printed line will be,
     * f(n = i) = fibonacci(i).
     *
     * For example, for the 6th fibonacci number,
-    * f(n = 6) = 8.
+    * f(n = 5) = 8.
     *
     * Param: fn_to_test: fn(u32) -> u32
      */
-
-    for n in 1..11 {
+    for n in 0..10 {
         println!("f(n = {}) = {}", n, fn_to_test(n));
     }
 }
