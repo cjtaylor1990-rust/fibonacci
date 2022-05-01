7534122334a8b471f923aa98562d268c306b2589
cjtaylor1990 <9288608+cjtaylor1990@users.noreply.github.com>
2022-05-01 08:41:12 -0400

    Gernalizing test and adding recursive Fibonacci.
    Adding a new recursive fibonacci calculator, and
    generalized test_fibonacci to take in a function
    as an argument.
    
    This is part of Ch.3 of TRPL.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
-    test_fibonacci();
+    // Testing for loop fibonacci
+    println!("For-loop fibonacci calculation:");
+    test_fibonacci(fibonacci);
+
+    // Testing recursive fibonacci
+    println!("Recursive fibonacci calculation:");
+    test_fibonacci(fibonacci_recursive);
+
 }
 
 fn fibonacci(n: u32) -> u32 {
+    // Fibonacci calculation using for-loop.
     let mut previous = 1;
     let mut current = 1;
 
@@ -15,7 +23,21 @@ fn fibonacci(n: u32) -> u32 {
     current
 }
 
-fn test_fibonacci() {
+fn fibonacci_recursive(n: u32) -> u32 {
+    // Fibonacci calculation using recursion.
+    if n == 1 || n == 2 {
+        return 1
+    }
+    fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+}
+
+fn test_fibonacci(fibonacci: fn(u32) -> u32) {
+    /*
+    * For a given fibonacci calculator, print the
+    * first 10 fibonacci numbers.
+    *
+    * Param: fibonacci: fn(u32) -> u32
+     */
     println!("First: {}", fibonacci(1));
     println!("Second: {}", fibonacci(2));
     println!("Third: {}", fibonacci(3));
